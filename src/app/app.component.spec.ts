13e9e9de9bf5cb88302f72e6a4b8d97c36159425
Kesavan Vinod Kumar <107707449+tourist1642@users.noreply.github.com>
2024-05-19 12:33:29 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -13,14 +12,6 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
-=======
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
->>>>>>> origin/main
   }));
 
   it('should create the app', () => {
@@ -29,28 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have as title 'fyle-githublist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('fyle-githublist');
-=======
-  it(`should have as title 'fyle-frontend-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('fyle-frontend-challenge');
->>>>>>> origin/main
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-<<<<<<< HEAD
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('fyle-githublist app is running!');
-=======
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fyle-frontend-challenge app is running!');
->>>>>>> origin/main
   });
 });
